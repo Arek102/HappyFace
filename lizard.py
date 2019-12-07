223751aa31b6dc4775a8baf3d40a8c661d5bdd6c
Arek Juljulian <arekjul@Areks-MacBook-Pro.local>
2019-12-07 10:55:59 -0800

    learning print function

--- lizard.py
+++ lizard.py
@@ -1,1 +1,7 @@
-print("zutritt verboten")
+print("ok boomer")
+print("olive is a big fatass who eats kibbles all day")
+
+print("olive says,\"Im not fat im just pleasurly plump\"")
+print("abc\ndef")
+print("ghi\tjkl")
+print("ghi  jkl")
