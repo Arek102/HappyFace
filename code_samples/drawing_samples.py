13cee25bea9dae0ca30c0f9909270287df1af7f6
Arek Juljulian <arekjul@Areks-MacBook-Pro.local>
2019-12-08 15:58:19 -0800

    drawing primitives

--- code_samples/drawing_samples.py
+++ code_samples/drawing_samples.py
@@ -5,11 +5,17 @@ this is line 2 of my comment
 # import the arcade library
 import arcade
 
-arcade.open_window(600,600,"drawing example")
+arcade.open_window(600, 600, "drawing example")
 
 arcade.set_background_color(arcade.color.AIR_SUPERIORITY_BLUE)
 
 arcade.start_render()
+
+# draw a rectangle
+# order is: left, right, top, bottom
+arcade.draw_lrtb_rectangle_filled(200, 520, 45, 25, arcade.color.BITTER_LIME)
+arcade.draw_rectangle_filled(200, 520, 45, 25, arcade.color.BITTER_LEMON, 45)
+
 arcade.finish_render()
 
 # keep the window open until someone closes it
