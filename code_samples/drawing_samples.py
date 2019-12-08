52a67d769e0c1faec4ebf51c333a1c23e1da2159
Arek Juljulian <arekjul@Areks-MacBook-Pro.local>
2019-12-07 19:31:25 -0800

    open empty window

--- code_samples/drawing_samples.py
+++ code_samples/drawing_samples.py
@@ -2,5 +2,17 @@
 this is line 1 of my comment
 this is line 2 of my comment
 """
+# import the arcade library
+import arcade
 
-# this is a single-line commennt
+arcade.open_window(600,600,"drawing example")
+
+arcade.set_background_color(arcade.color.AIR_SUPERIORITY_BLUE)
+
+arcade.start_render()
+arcade.finish_render()
+
+# keep the window open until someone closes it
+arcade.run()
+
+
